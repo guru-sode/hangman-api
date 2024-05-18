6f7066ada2785a3063e5d049f9313511bb4643f5
Gurukiran <guru.sode@gmail.com>
2024-05-18 15:48:08 +0530

    Create server

--- index.js
+++ index.js
@@ -1,17 +1,57 @@
-const express = require ('express'); 
-  
-const app = express(); 
-const PORT = 3000; 
-  
-app.get('/', (req, res)=>{ 
-    res.status(200); 
-    res.send("Welcome to root URL of Server"); 
-}); 
-  
-app.listen(PORT, (error) =>{ 
-    if(!error) 
-        console.log("Server is Successfully Running and App is listening on port "+ PORT) 
-    else 
-        console.log("Error occurred, server can't start", error); 
-    } 
-); 
+const express = require('express');
+const cors = require('cors');
+const axios = require('axios');
+const { v4: uuidv4 } = require('uuid');
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+const games = {};
+
+const getRandomWord = async () => {
+  try {
+    const response = await axios.get('https://random-word-api.herokuapp.com/word?number=1');
+    return response.data[0];
+  } catch (error) {
+    console.error(error);
+    return 'default';
+  }
+};
+
+app.post('/start', async (req, res) => {
+  const gameId = uuidv4();
+  const word = await getRandomWord();
+  games[gameId] = {
+    word,
+    guesses: [],
+    wrongGuesses: 0,
+  };
+  console.log(games);
+  res.json({ gameId, wordLength: word.length });
+});
+
+app.post('/guess', (req, res) => {
+  const { gameId, letter } = req.body;
+  const game = games[gameId];
+  if (!game) return res.status(404).send('Game not found');
+
+  if (game.word.includes(letter)) {
+    game.guesses.push(letter);
+  } else {
+    game.wrongGuesses += 1;
+  }
+
+  const revealedWord = game.word.split('').map(l => (game.guesses.includes(l) ? l : '_')).join('');
+
+  res.json({
+    revealedWord,
+    wrongGuesses: game.wrongGuesses,
+    isGameOver: game.wrongGuesses >= 6 || !revealedWord.includes('_'),
+    isWin: !revealedWord.includes('_'),
+  });
+});
+
+app.listen(5000, () => {
+  console.log('Server running on port 5000');
+});
